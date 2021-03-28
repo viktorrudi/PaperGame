257e871b6b084a861faaffc5df543256a9c942fd
Viktor Rudi <rudi@Viktors-MacBook-Pro.local>
2021-03-28 20:12:19 +0200

    Upgraded SDK

--- App.js
+++ App.js
@@ -2,14 +2,14 @@
 import React from "react";
 import firebase from "firebase";
 import { Provider } from "react-redux";
-import { YellowBox } from "react-native";
+import { LogBox } from "react-native";
 import AppNavigator from "./AppNavigator";
 
 import firebaseConfig from "./config";
 
 import { store } from "./store";
 
-YellowBox.ignoreWarnings(["Setting a timer for a long period of time"]);
+LogBox.ignoreLogs(["Setting a timer for a long period of time"]);
 
 // Initialize once
 if (firebase && firebase.apps.length === 0) {
